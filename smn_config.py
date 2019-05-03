a29750bb21426b1881ef694a24c6e8b7b5fa9b03
Damita Zweiback <43229527+DZweiback@users.noreply.github.com>
2019-05-02 22:22:46 -0400

    Delete smn_config.py

--- smn_config.py
+++
@@ -1,7 +0,0 @@
-# Cloud AWS Database Info - for Say-My-Name-Say-My-Name
-remote_db_endpoint = 'endpoint'
-remote_db_port = 'xxxx'
-remote_dbname = 'saymyname'
-remote_dbuser = 'me'
-remote_dbpwd = 'password'
-
